82d701678adab26ed86edb3e34ae4d768dc3f3d8
Jarosław Maciejczak <michallo0811@gmail.com>
2016-05-08 22:27:41 +0200

    configloggers modified

--- src/com/luxoft/bankapp/service/ConfigLoggers.java
+++ src/com/luxoft/bankapp/service/ConfigLoggers.java
@@ -1,6 +1,7 @@
 package com.luxoft.bankapp.service;
 
 import java.io.IOException;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
@@ -11,13 +12,16 @@ public class ConfigLoggers {
 			Logger la = Logger.getLogger("LogA");
 			Logger lb = Logger.getLogger("LogB");
 			Logger lc = Logger.getLogger("LogC");
-			try {
-				la.addHandler(new FileHandler("db.log"));
-				lb.addHandler(new FileHandler("clients.log"));
-				lc.addHandler(new FileHandler("exceptions.log"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			la.addHandler(new ConsoleHandler());
+			lb.addHandler(new ConsoleHandler());
+			lc.addHandler(new ConsoleHandler());
+//			try {
+//				la.addHandler(new FileHandler("db.log"));
+//				lb.addHandler(new FileHandler("clients.log"));
+//				lc.addHandler(new FileHandler("exceptions.log"));
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		}
